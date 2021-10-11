36b2c9dc25bbb5a0fc4f9d7ce476beaea98c1e9d
Alexandros Stergiou <jutaroroche@gmail.com>
2021-10-11 13:42:26 +0200

    First setup

--- index.js
+++ index.js
@@ -1,46 +1,14 @@
-const fs = require('fs');
-const {Client, Intents, Collection, MessageEmbed} = require('discord.js');
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]});
-const CONFIG = require('./configs/config.json');
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./configs/config.json');
 
-client.prefix = CONFIG.prefix;
-client.botCommands = new Collection();
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// ERROR MESSAGE
-client.error = (error_msg, channel) => {
-    if (!error_msg || !channel) return;
-    const error_embed = new MessageEmbed()
-        .setTitle('❌ An Error occured!')
-        .setColor('RED')
-        .setDescription(`\`\`\`${error_msg}\`\`\``)
-        .setTimestamp();
-    return channel.send({embeds: [error_embed]}).catch(e => console.log(`Couldn't send error embed!\n${e}`));
-};
-
-//EVENT HANDLER
-fs.readdir('./events/', (error, files) => {
-    if (error) return console.error(error);
-    client.removeAllListeners();
-    files.forEach(file => {
-        if (fs.lstatSync(`./events/${file}`).isDirectory()) return;
-        const event = require(`./events/${file}`);
-        const event_name = file.split('.')[0];
-        try {
-            client.on(event_name, event.bind(null, client));
-        } catch (e) {
-            console.log(e);
-        }
-    });
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
 });
 
-//COMMAND HANDLER
-fs.readdir('./commands/', (error, files) => {
-    if (error) return console.error(error);
-    const command_files = files.filter(fileName => fileName.endsWith('.js'));
-    command_files.forEach(file => {
-        const command = require(`./commands/${file}`);
-        client.botCommands.set(command.name, command);
-    });
-});
-
-client.login(CONFIG.token);
+// Login to Discord with your client's token
+client.login(token);
