b012e87b22eb654b925eaaeb64350f8f2ebc44a1
Alexandros Stergiou <jutaroroche@gmail.com>
2021-10-13 18:03:07 +0200

    Correctly implemented dice subcommands as well as split big string. Dice skill must be expanded

--- commands/subcommands/dice_skill.js
+++ commands/subcommands/dice_skill.js
@@ -1,7 +1,10 @@
+const dice_roller = require('../../utils/dice_roller');
 
 module.exports = {
     async execute(interaction) {
         const name=interaction.options.getString('name');
-        await interaction.reply('Name: '+name);
+        await interaction.deferReply({ ephemeral: true });
+        const dice_roll=dice_roller.dice_roll(20);
+        await interaction.editReply(name+' rolled: '+dice_roll);
     },
 };
